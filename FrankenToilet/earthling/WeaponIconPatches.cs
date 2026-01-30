34199b40eba1cd3e4ec2f38ac493151ce62932bb
earthlingOnFire <78956999+earthlingOnFire@users.noreply.github.com>
2026-01-30 23:51:18 +0000

    feat: replace all weapon icons with an awesome one

---
+++ FrankenToilet/earthling/WeaponIconPatches.cs
@@ -0,0 +1,25 @@
+﻿using FrankenToilet.Core;
+using HarmonyLib;
+
+namespace FrankenToilet.earthling;
+
+[PatchOnEntry]
+[HarmonyPatch(typeof(WeaponIcon))]
+public static class WeaponIconPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch("Start")]
+    public static void ReplaceIcon(WeaponIcon __instance)
+    {
+        WeaponDescriptor? awesomeGun = AssetBundleHelper.LoadAsset<WeaponDescriptor>("Assets/Bundles/toiletonfire/awesomegun.asset");
+
+        if (awesomeGun == null)
+        {
+            LogHelper.LogError("Could not load awesome weapon icons");
+            return;
+        }
+
+        __instance.weaponDescriptor.icon = awesomeGun.icon;
+        __instance.weaponDescriptor.glowIcon = awesomeGun.glowIcon;
+    }
+}
